c54693802c87212d1562e306be6ebd2cdc070540
UnifiedAI-ONeID <simon.luke@ufv.ca>
2026-01-17 00:16:07 -0800

    fixes to isar

--- lib/dataconnect_generated/generated.dart
+++ lib/dataconnect_generated/generated.dart
@@ -1,15 +1,13 @@
 library dataconnect_generated;
-import 'package:firebase_data_connect/firebase_data_connect.dart';
-import 'package:flutter/foundation.dart';
 import 'dart:convert';
 
+import 'package:firebase_data_connect/firebase_data_connect.dart';
+import 'package:flutter/foundation.dart';
+
 part 'create_demo_user.dart';
-
 part 'list_all_teams.dart';
-
-part 'update_player_jersey_number.dart';
-
 part 'list_my_teams.dart';
+part 'update_player_jersey_number.dart';
 
 
 
